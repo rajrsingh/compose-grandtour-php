898126236fd3e0242fdbd4e653d4de4332294f4b
Raj Singh <rrsingh@us.ibm.com>
2017-10-07 23:06:52 -0400

    first version of postgresql example

---
+++ example-postgresql/env.template.php
@@ -0,0 +1,8 @@
+<?php
+// rename this file env.php and insert the proper values for your database
+$dbuser = 'database user name';
+$dbpass = 'database password';
+$host = 'something-long-goes-here.dblayer.com';
+$dbname='some database name, e.g. compose';
+$port='some port';
+?>
