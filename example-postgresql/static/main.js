d48575790dc53ab55636dc247a50465db2fb6e08
Lorna Jane Mitchell <lorna@lornajane.net>
2017-11-22 17:35:54 +0000

    Modernisation and better URL parsing
    Moved config to a file called config rather than env, set up project
    and README to expect to be served by built in webserver.  PostgreSQL URL
    now parsed by parse_url() for neatness

--- example-postgresql/static/main.js
+++ example-postgresql/static/main.js
@@ -3,12 +3,14 @@ $(document).ready(function() {
   function reload() {
     $('.hidden').fadeOut();
     $('displayOutput').empty();
-    $.getJSON( '../example-postgresql.php').done(function(data) {
+    $.getJSON( 'words').done(function(data) {
       console.log("showing", data);
       var rendered = "<ul>";
-      data.items.forEach(function(item) {
-        rendered = rendered + "<li>The word <b>" + item.word + "</b> is defined as <b>" + item.definition + "</b></li>";
-      });
+      if(data.length) {
+        data.forEach(function(item) {
+          rendered = rendered + "<li>The word <b>" + item.word + "</b> is defined as <b>" + item.definition + "</b></li>";
+        });
+      }
       rendered = rendered + "</ul>";
 
       $('#displayOutput').html(rendered);
@@ -19,7 +21,7 @@ $(document).ready(function() {
   $('#add-word').submit(function(e) {
     e.preventDefault();
     $.ajax({
-      url: '../example-postgresql.php',
+      url: 'words',
       type: 'PUT',
       data: $(this).serialize(),
       success: function(data) {
